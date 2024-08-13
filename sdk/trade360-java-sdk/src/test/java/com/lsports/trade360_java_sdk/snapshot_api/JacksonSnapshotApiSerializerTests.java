0e21159f77967a13961606530e9698930f15f17b
Jakub Michałek <j.michalek@statscore.com>
2024-08-13 17:08:41 +0200

    Aligned SnapshotAPI model with the refactored Snapshot API repo

--- sdk/trade360-java-sdk/src/test/java/com/lsports/trade360_java_sdk/snapshot_api/JacksonSnapshotApiSerializerTests.java
+++ sdk/trade360-java-sdk/src/test/java/com/lsports/trade360_java_sdk/snapshot_api/JacksonSnapshotApiSerializerTests.java
@@ -12,7 +12,7 @@ import java.util.List;
 import org.junit.Test;
 
 import com.lsports.trade360_java_sdk.snapshot_api.configuration.SnapshotApiSettings;
-import com.lsports.trade360_java_sdk.snapshot_api.entities.requests.GetFixturesRequestDto;
+import com.lsports.trade360_java_sdk.snapshot_api.entities.requests.GetSnapshotRequest;
 
 public class JacksonSnapshotApiSerializerTests {
         private final SnapshotApiSettings apiSettings = new SnapshotApiSettings(
@@ -25,7 +25,9 @@ public class JacksonSnapshotApiSerializerTests {
     public void serialize_whenEmptyRequestProvided_appendsCredentialsToFinalJson() {
         // Arrange
         var serializer = new JacksonSnapshotApiSerializer(this.apiSettings);
-        var request = new GetFixturesRequestDto(
+        var request = new GetSnapshotRequest(
+            null,
+            null,
             null,
             null,
             null,
@@ -49,14 +51,16 @@ public class JacksonSnapshotApiSerializerTests {
     public void serialize_whenNonEmptyRequestProvided_appendsCredentialsToFinalJson() {
         // Arrange
         var serializer = new JacksonSnapshotApiSerializer(this.apiSettings);
-        var request = new GetFixturesRequestDto(
+        var request = new GetSnapshotRequest(
+            ZonedDateTime.of(LocalDateTime.of(2024, 8, 5, 0, 0), ZoneId.of(ZoneOffset.UTC.getId())),
             ZonedDateTime.of(LocalDateTime.of(2024, 8, 1, 0, 0), ZoneId.of(ZoneOffset.UTC.getId())),
             ZonedDateTime.of(LocalDateTime.of(2024, 8, 7, 0, 0), ZoneId.of(ZoneOffset.UTC.getId())),
             List.of(1, 2, 3),
             List.of(4, 5, 6),
             List.of(7, 8, 9),
             List.of(10, 11, 12),
-            List.of(13, 14, 15)
+            List.of(13, 14, 15),
+            List.of(16, 17, 18)
         );
 
         // Act
@@ -64,7 +68,7 @@ public class JacksonSnapshotApiSerializerTests {
 
         // Assert
         assertEquals(
-            "{\"FromDate\":1722470400000,\"ToDate\":1722988800000,\"Sports\":[1,2,3],\"Locations\":[4,5,6],\"Fixtures\":[7,8,9],\"Leagues\":[10,11,12],\"Markets\":[13,14,15],\"PackageId\":1234,\"UserName\":\"testUser\",\"Password\":\"testPassword\"}",
+            "{\"Timestamp\":1722816000000,\"FromDate\":1722470400000,\"ToDate\":1722988800000,\"Sports\":[1,2,3],\"Locations\":[4,5,6],\"Leagues\":[7,8,9],\"Tournaments\":[10,11,12],\"Fixtures\":[13,14,15],\"Markets\":[16,17,18],\"PackageId\":1234,\"UserName\":\"testUser\",\"Password\":\"testPassword\"}",
             result.toString()
         );
     }
