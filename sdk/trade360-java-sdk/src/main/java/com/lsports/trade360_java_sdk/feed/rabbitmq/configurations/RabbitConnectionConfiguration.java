b2fe6d59eb16cc173a0318b02604ad4921d3b00c
AndrzejKoziel <andrzej.k@lsports.eu>
2024-08-27 12:56:24 +0200

    Add configuration properties validation

--- sdk/trade360-java-sdk/src/main/java/com/lsports/trade360_java_sdk/feed/rabbitmq/configurations/RabbitConnectionConfiguration.java
+++ sdk/trade360-java-sdk/src/main/java/com/lsports/trade360_java_sdk/feed/rabbitmq/configurations/RabbitConnectionConfiguration.java
@@ -1,29 +1,50 @@
 package com.lsports.trade360_java_sdk.feed.rabbitmq.configurations;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @Data
 public class RabbitConnectionConfiguration {
 
+
     public String name;
+    @NotNull
     public int package_id;
+    @NotBlank
     public String host = "localhost";
+    @NotNull
     public int port = 5672;
+    @NotBlank
     public String virtual_host = "/";
+    @NotBlank
     public String user_name = "guest";
+    @NotBlank
     public String password = "guest";
+    @NotNull
     public int prefetch_count;
+    @NotNull
     public boolean auto_ack = true;
+    @NotNull
     public int requested_heartbeat_seconds = 30;
+    @NotNull
     public int network_recovery_interval = 30;
-    public boolean dispatch_consumers_async = true;
-    public boolean automatic_recovery_enabled = true;
+    @NotBlank
     public String base_customers_api;
+    @NotNull
     public int retry_attempts = 3;
+    @NotNull
     public int retry_initial_interval = 1000;
+    @NotNull
     public double retry_multiple = 2;
+    @NotNull
     public int retry_max_interval = 5000;
+    @NotNull
     public int concurrent_consumers = 1;
+    @NotNull
     public int max_concurrent_consumers = 20;
+    @NotBlank
     public String rabbit_listener_container_factory_name;
 }
