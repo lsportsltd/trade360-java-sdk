48b041960c8b3dec0284310e36f6e17a98c4f238
Jakub Michałek <j.michalek@statscore.com>
2024-09-09 11:46:02 +0200

    minor

--- samples/trade360-samples/src/main/java/com/lsports/trade360_metadata_api_example/MetadataApiExampleApplication.java
+++ samples/trade360-samples/src/main/java/com/lsports/trade360_metadata_api_example/MetadataApiExampleApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lsports.trade360_java_sdk.common.configuration.PackageCredentials;
