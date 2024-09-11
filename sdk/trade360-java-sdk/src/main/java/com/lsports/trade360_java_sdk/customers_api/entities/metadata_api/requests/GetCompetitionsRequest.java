1b7baf106486c7ab45ae85824b087b971bdf509d
Jakub Michałek <j.michalek@statscore.com>
2024-09-11 15:13:09 +0200

    MON-1583 Added GetCompetitions endpoint

---
+++ sdk/trade360-java-sdk/src/main/java/com/lsports/trade360_java_sdk/customers_api/entities/metadata_api/requests/GetCompetitionsRequest.java
@@ -0,0 +1,12 @@
+package com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.requests;
+
+import com.lsports.trade360_java_sdk.common.entities.enums.SubscriptionState;
+
+import jakarta.annotation.Nullable;
+
+public final record GetCompetitionsRequest(
+    @Nullable Iterable<Integer> sportIds,
+    @Nullable Iterable<Integer> locationIds,
+    @Nullable Iterable<Integer> competitionIds,
+    SubscriptionState subscriptionStatus) {
+}
--- sdk/trade360-java-sdk/src/main/java/com/lsports/trade360_java_sdk/customers_api/interfaces/MetadataApiClient.java
+++ sdk/trade360-java-sdk/src/main/java/com/lsports/trade360_java_sdk/customers_api/interfaces/MetadataApiClient.java
@@ -1,11 +1,13 @@
 package com.lsports.trade360_java_sdk.customers_api.interfaces;
 
 
+import com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.requests.GetCompetitionsRequest;
 import com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.requests.GetLeaguesRequest;
 import com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.requests.GetLocationsRequest;
 import com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.requests.GetMarketsRequest;
 import com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.requests.GetSportsRequest;
 import com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.requests.GetTranslationsRequest;
+import com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.responses.GetCompetitionsResponse;
 import com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.responses.GetLeaguesResponse;
 import com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.responses.GetLocationsResponse;
 import com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.responses.GetMarketsResponse;
@@ -28,4 +30,6 @@ public interface MetadataApiClient {
     Mono<GetMarketsResponse> getMarkets(GetMarketsRequest request);
 
     Mono<GetTranslationsResponse> getTranslations(GetTranslationsRequest request);
+
+    Mono<GetCompetitionsResponse> getCompetitions(GetCompetitionsRequest request);
 }
