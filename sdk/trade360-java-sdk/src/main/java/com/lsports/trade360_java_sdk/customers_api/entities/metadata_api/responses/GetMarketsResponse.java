c51e3a10b03e75a71e77074978170c2768b1b449
Jakub Michałek <j.michalek@statscore.com>
2024-09-11 10:56:58 +0200

    MON-1581 GetMarkets

---
+++ sdk/trade360-java-sdk/src/main/java/com/lsports/trade360_java_sdk/customers_api/entities/metadata_api/responses/GetMarketsResponse.java
@@ -0,0 +1,6 @@
+package com.lsports.trade360_java_sdk.customers_api.entities.metadata_api.responses;
+
+import jakarta.annotation.Nullable;
+
+public final record GetMarketsResponse(@Nullable Iterable<MarketMetadata> markets) {
+}
