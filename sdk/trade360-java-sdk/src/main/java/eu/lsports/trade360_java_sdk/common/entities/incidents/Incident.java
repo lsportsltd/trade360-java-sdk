513f791d50521feefbcdf503787003af01b6e9a8
shir.b <shir.b@lsports.eu>
2025-05-27 12:26:55 +0300

    Enhance Incident class with additional attributes and documentation; fix Javadoc reference in MetadataApiClient interface.

--- sdk/trade360-java-sdk/src/main/java/eu/lsports/trade360_java_sdk/common/entities/incidents/Incident.java
+++ sdk/trade360-java-sdk/src/main/java/eu/lsports/trade360_java_sdk/common/entities/incidents/Incident.java
@@ -4,12 +4,22 @@ import jakarta.annotation.Nullable;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents an incident in the system.
+ */
 public class Incident {
+    /** The sport ID associated with the incident. */
     public Integer sportId;
+    /** The sport name associated with the incident. */
     public String sportName;
+    /** The unique incident ID. */
     public Integer incidentId;
+    /** The name of the incident. */
     public String incidentName;
+    /** The description of the incident, if available. */
     @Nullable public String description;
+    /** The last update timestamp for the incident. */
     public LocalDateTime lastUpdate;
+    /** The creation date of the incident. */
     public LocalDateTime creationDate;
 }
