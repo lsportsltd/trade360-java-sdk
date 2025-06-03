47b89ae3cb0e5fa4efd8ceae19e092683fb1d5b0
shir.b <shir.b@lsports.eu>
2025-06-03 15:25:55 +0300

    adding more tests

--- sdk/trade360-java-sdk/src/test/java/eu/lsports/trade360_java_sdk/common/entities/livescore/ScoreboardTest.java
+++ sdk/trade360-java-sdk/src/test/java/eu/lsports/trade360_java_sdk/common/entities/livescore/ScoreboardTest.java
@@ -11,13 +11,13 @@ class ScoreboardTest {
     void testFieldAssignment() {
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.status = FixtureStatus.FINISHED;
-        scoreboard.description = StatusDescription.FINISHED;
+        scoreboard.description = StatusDescription.NONE;
         scoreboard.currentPeriod = 2;
         scoreboard.time = "90:00";
         Result result = new Result();
         scoreboard.results = List.of(result);
         assertEquals(FixtureStatus.FINISHED, scoreboard.status);
-        assertEquals(StatusDescription.FINISHED, scoreboard.description);
+        assertEquals(StatusDescription.NONE, scoreboard.description);
         assertEquals(2, scoreboard.currentPeriod);
         assertEquals("90:00", scoreboard.time);
         assertNotNull(scoreboard.results);
