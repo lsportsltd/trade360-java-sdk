be1bf60d691a1e397ad5414d9f23e5beda2238d9
Amit Cohen <168630601+amitccohenLS@users.noreply.github.com>
2025-06-10 14:04:10 +0300

    update

--- sdk/trade360-java-sdk/src/test/java/eu/lsports/trade360_java_sdk/common/entities/markets/ProviderMarketBetBusinessTest.java
+++ sdk/trade360-java-sdk/src/test/java/eu/lsports/trade360_java_sdk/common/entities/markets/ProviderMarketBetBusinessTest.java
@@ -17,14 +17,12 @@ class ProviderMarketBetBusinessTest {
         bet.id = 101;
         bet.name = "Provider Special";
         bet.line = "3.5";
-        bet.isChanged = 0;
         bet.probability = 0.75;
         bet.participantId = 99;
         bet.playerName = "Provider Player";
         assertEquals(101, bet.id);
         assertEquals("Provider Special", bet.name);
         assertEquals("3.5", bet.line);
-        assertEquals(0, bet.isChanged);
         assertEquals(0.75, bet.probability);
         assertEquals(99, bet.participantId);
         assertEquals("Provider Player", bet.playerName);
