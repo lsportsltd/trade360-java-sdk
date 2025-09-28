cc1982242b304003435c279f1a59868c03f6d4b4
noammerLS <noam.m@lsports.eu>
2025-09-28 11:09:56 +0300

    Update IdNamePair.java
    To Record

--- sdk/trade360-java-sdk/src/main/java/eu/lsports/trade360_java_sdk/common/entities/shared/IdNamePair.java
+++ sdk/trade360-java-sdk/src/main/java/eu/lsports/trade360_java_sdk/common/entities/shared/IdNamePair.java
@@ -3,16 +3,6 @@ package eu.lsports.trade360_java_sdk.common.entities.shared;
 /**
  * The {@code IdNamePair} class represents an id-name pair.
  */
-public class IdNamePair {
-
-    /**
-     * The id in the id-name pair.
-     */
-    public Integer id;
-
-    /**
-     * The name in the id-name pair.
-     */
-    public String name;
+public record IdNamePair(Integer id, String name) {
 }
 
